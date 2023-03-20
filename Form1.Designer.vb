df1eb257d3f7a660b6ba902ae63c5da2675b02bb
Eng. Kirolos Mayiz Fahem <72953174+KirolosMFahem@users.noreply.github.com>
2023-03-20 22:20:00 +0200

    Add files via upload

---
+++ Form1.Designer.vb
@@ -0,0 +1,95 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Label1 = New Label()
+        Label2 = New Label()
+        BirthDate = New DateTimePicker()
+        CurrentDate = New DateTimePicker()
+        btnCalculateAge = New Button()
+        SuspendLayout()
+        ' 
+        ' Label1
+        ' 
+        Label1.AutoSize = True
+        Label1.Font = New Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Label1.Location = New Point(29, 47)
+        Label1.Name = "Label1"
+        Label1.Size = New Size(71, 32)
+        Label1.TabIndex = 0
+        Label1.Text = "BDay"' 
+        ' Label2
+        ' 
+        Label2.AutoSize = True
+        Label2.Font = New Font("Segoe UI Semibold", 18F, FontStyle.Bold, GraphicsUnit.Point)
+        Label2.Location = New Point(29, 115)
+        Label2.Name = "Label2"
+        Label2.Size = New Size(70, 32)
+        Label2.TabIndex = 1
+        Label2.Text = "PDay"' 
+        ' BirthDate
+        ' 
+        BirthDate.Location = New Point(123, 56)
+        BirthDate.Name = "BirthDate"
+        BirthDate.Size = New Size(200, 23)
+        BirthDate.TabIndex = 2
+        ' 
+        ' CurrentDate
+        ' 
+        CurrentDate.Location = New Point(123, 124)
+        CurrentDate.Name = "CurrentDate"
+        CurrentDate.Size = New Size(200, 23)
+        CurrentDate.TabIndex = 3
+        ' 
+        ' btnCalculateAge
+        ' 
+        btnCalculateAge.Font = New Font("Segoe UI Semibold", 14.25F, FontStyle.Bold, GraphicsUnit.Point)
+        btnCalculateAge.Location = New Point(152, 307)
+        btnCalculateAge.Name = "btnCalculateAge"
+        btnCalculateAge.Size = New Size(101, 37)
+        btnCalculateAge.TabIndex = 4
+        btnCalculateAge.Text = "Calc"
+        btnCalculateAge.UseVisualStyleBackColor = True
+        ' 
+        ' Form1
+        ' 
+        AutoScaleDimensions = New SizeF(7F, 15F)
+        AutoScaleMode = AutoScaleMode.Font
+        ClientSize = New Size(416, 450)
+        Controls.Add(btnCalculateAge)
+        Controls.Add(CurrentDate)
+        Controls.Add(BirthDate)
+        Controls.Add(Label2)
+        Controls.Add(Label1)
+        Name = "Form1"
+        Text = "Form1"
+        ResumeLayout(False)
+        PerformLayout()
+    End Sub
+
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents BirthDate As DateTimePicker
+    Friend WithEvents CurrentDate As DateTimePicker
+    Friend WithEvents btnCalculateAge As Button
+End Class
---
+++ Form1.vb
@@ -0,0 +1,21 @@
+﻿Public Class Form1
+
+    Private Sub btnCalculateAge_Click(sender As Object, e As EventArgs) Handles btnCalculateAge.Click
+        Dim birthDate As DateTime = birthDate.Value
+        Dim currentDate As DateTime = currentDate.Value
+
+        Dim age As TimeSpan = currentDate.Subtract(birthDate)
+
+        Dim ageYears As Integer = age.Days \ 365
+        age = age - TimeSpan.FromDays(ageYears * 365)
+
+        Dim ageMonths As Integer = age.Days \ 30
+        age = age - TimeSpan.FromDays(ageMonths * 30)
+
+        Dim ageDays As Integer = age.Days
+
+        MessageBox.Show($"Age: {ageYears} years, {ageMonths} months, {ageDays} days")
+    End Sub
+
+
+End Class
